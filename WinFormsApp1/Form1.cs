81e579c0f84939ec5281047a37690fc739906a9c
keshavsoft <nknnkumar@yahoo.com>
2022-11-21 08:33:53 +0530

    keshav

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -12,7 +12,7 @@ namespace WinFormsApp1
 
         private async void button1_Click(object sender, EventArgs e)
         {
-            richTextBox1.Text = await Example();
+            richTextBox1.Text = await ExampleGet();
         }
 
         public async Task<String> Example()
@@ -20,8 +20,8 @@ namespace WinFormsApp1
             //The data that needs to be sent. Any object works.
             var pocoObject = new
             {
-                Name = "John Doe",
-                Occupation = "gardener"
+                Name = "KeshavSoft",
+                Occupation = "Developers"
             };
 
             //Converting the object to a json string. NOTE: Make sure the object doesn't contain circular references.
@@ -31,7 +31,10 @@ namespace WinFormsApp1
             StringContent data = new StringContent(json, Encoding.UTF8, "application/json");
 
             //The url to post to.
-            var url = "https://httpbin.org/post";
+            //var url = "https://httpbin.org/post";
+
+
+            var url = "https://localhost:4151";
             var client = new HttpClient();
 
             //Pass in the full URL and the json string content
@@ -45,6 +48,41 @@ namespace WinFormsApp1
 
             return result;
         }
+
+        public async Task<String> ExampleGet()
+        {
+            //The data that needs to be sent. Any object works.
+            var pocoObject = new
+            {
+                Name = "KeshavSoft",
+                Occupation = "Developers"
+            };
+
+            //Converting the object to a json string. NOTE: Make sure the object doesn't contain circular references.
+            string json = JsonConvert.SerializeObject(pocoObject);
+
+            //Needed to setup the body of the request
+            StringContent data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            //The url to post to.
+            //var url = "https://httpbin.org/post";
+
+
+            var url = "http://localhost:4119";
+            var client = new HttpClient();
+
+            //Pass in the full URL and the json string content
+            var response = await client.GetAsync(url);
+
+            //It would be better to make sure this request actually made it through
+            string result = await response.Content.ReadAsStringAsync();
+
+            //close out the client
+            client.Dispose();
+
+            return result;
+        }
+
 
 
     }
